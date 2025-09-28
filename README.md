396e4113b26fe11bfdf44bd19f0802dc421996d0
veygax <veyga@veygax.dev>
2025-09-28 23:04:57 +0100

    use millennium sdk

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 a [millennium](https://steambrew.app) plugin to enable the "Valve Internal" menu typically hidden for non-valve employees.
 
-it works by running the following:
+it works by running the following (but using millennium apis):
 ```js
 window.webpackChunksteamui.push([[Math.random()], {}, __webpack_require__ => {
   __webpack_require__(34792).rV.m_Settings.bIsValveEmail = true;
+  __webpack_require__(34792).TS.DEV_MODE = true;
 }]);
 ```
 
--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,10 +1,8 @@
-import { Millennium } from "@steambrew/client";
+import { Millennium, modules } from "@steambrew/client";
 
 export default async function PluginMain() {
   Millennium.AddWindowCreateHook(async () => {
-    (window as any).webpackChunksteamui.push([[Math.random()], {}, (__webpack_require__: any) => {
-      __webpack_require__(34792).rV.m_Settings.bIsValveEmail = true;
-      (window as any).SteamClient._internal?.SetDevMode(true);
-    }]);
+    modules.get("34792").rV.m_Settings.bIsValveEmail = true;
+    modules.get("72476").TS.DEV_MODE = true;
   });
 }
